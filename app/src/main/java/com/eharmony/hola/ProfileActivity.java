e429bb870415957667a86a44ebe249ca4f35da6e
Junu Kang <mail@ijunes.com>
2016-02-25 20:41:29 -0800

    some fancy animations

--- app/src/main/java/com/eharmony/hola/ProfileActivity.java
+++ app/src/main/java/com/eharmony/hola/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.eharmony.hola;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+
+import com.eftimoff.androidplayer.Player;
+import com.eftimoff.androidplayer.actions.property.PropertyAction;
 
 public class ProfileActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,8 @@ public class ProfileActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        animateSampleOne(toolbar, findViewById(R.id.content_profile), fab);
     }
 
     @Override
@@ -47,5 +56,16 @@ public class ProfileActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void animateSampleOne(View toolbar, View activityMainContentLayout, View activityMainPinkFab) {
+        final PropertyAction fabAction = PropertyAction.newPropertyAction(activityMainPinkFab).scaleX(0).scaleY(0).duration(750).interpolator(new AccelerateDecelerateInterpolator()).build();
+        final PropertyAction headerAction = PropertyAction.newPropertyAction(toolbar).interpolator(new DecelerateInterpolator()).translationY(-200).duration(550).alpha(0.4f).build();
+        final PropertyAction bottomAction = PropertyAction.newPropertyAction(activityMainContentLayout).translationY(500).duration(750).alpha(0f).build();
+        Player.init().
+                animate(headerAction).
+                then().
+                animate(bottomAction).
+                play();
     }
 }
